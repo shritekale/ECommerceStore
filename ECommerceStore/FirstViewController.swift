f524a6affc855e17e541f52c464b5fa06fc2bcf6
Shrikantreddy Tekale <Shrikantreddy.Tekale@derivco.co.uk>
2020-03-29 13:42:57 +0100

    Initial Commit

---
+++ ECommerceStore/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ECommerceStore
+//
+//  Created by Shrikantreddy Tekale on 29/03/2020.
+//  Copyright © 2020 Shrikantreddy Tekale. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ ECommerceStore/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ECommerceStore
+//
+//  Created by Shrikantreddy Tekale on 29/03/2020.
+//  Copyright © 2020 Shrikantreddy Tekale. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
