6d77faa2fffd9571fd596126739c63caba1d3f75
Shrikantreddy Tekale <Shrikantreddy.Tekale@derivco.co.uk>
2020-03-29 13:51:47 +0100

    Created empty project and added gitignore

---
+++ ECommerceStoreAPI/ECommerceStoreAPI.h
@@ -0,0 +1,19 @@
+//
+//  ECommerceStoreAPI.h
+//  ECommerceStoreAPI
+//
+//  Created by Shrikantreddy Tekale on 29/03/2020.
+//  Copyright © 2020 Shrikantreddy Tekale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ECommerceStoreAPI.
+FOUNDATION_EXPORT double ECommerceStoreAPIVersionNumber;
+
+//! Project version string for ECommerceStoreAPI.
+FOUNDATION_EXPORT const unsigned char ECommerceStoreAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ECommerceStoreAPI/PublicHeader.h>
+
+
